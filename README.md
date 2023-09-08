cf6d6c911b64c6c06c4cf2407da89dda3231652f
Halil İbrahim Kamay <ibrahimkamayweb@gmail.com>
2023-09-08 15:41:56 +0300

    updated

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# captcha-generator
- 
+
+# Javascript Custom Captcha Generator
+
+Bu projede websitelerinde sürekli olarak karşılaştığımız captcha generator yaptım. Refresh butonu veya sayfayı her yenilediğinizde yeni bir captcha oluşturabilirsiniz. Input'a girerek girdiğiniz kodu teyit edebilirsiniz. Doğru veya hatalı olarak mesaj alabilirsiniz. Reaksiyonlar'a göre gerekli özelleştirmeleri yaptım. 
+
+
+Önizleme;
+
+![Logo](https://i.hizliresim.com/ecc3qs8.jpg)
+
+    
--- app.js
+++ app.js
@@ -1,21 +1,55 @@
-//Dom elementlerini seçiyoruz.
-
+//Selecting necessary DOM elements
 const captchaTextBox = document.querySelector(".captcha_box input");
 const refreshButton = document.querySelector(".refresh_button");
 const captchaInputBox = document.querySelector(".captcha_input input");
 const message = document.querySelector(".message");
 const submitBtn = document.querySelector(".button");
 
-
+// Variable to store generated captcha
 let captchaText = null;
 
-//Captcha oluşturma fonksiyonunu yapıyoruz.
-
+// Function to generate captcha
 const generateCaptcha = () => {
     const randomString = Math.random().toString(36).substring(2,7);
     const randomStringArray = randomString.split("");
-    captchaTextBox.value = (randomString);
-    console.log(randomStringArray);
+    const changeString = randomStringArray.map(char => Math.random()> 0.5 ? char.toUpperCase() : char);
+    captchaText = changeString.join(" ")
+    console.log(captchaText);
+
+    captchaTextBox.value = (captchaText);
 }
 
+// addEventListener for the refresh button, captchaInputBox, submit
+const refreshBtnClick = () => {
+    generateCaptcha();
+    captchaInputBox.value = "";
+    captchaKeyUpValidate();
+}   
+
+// Toggle submit button disable class based on captcha input field
+const captchaKeyUpValidate = () => {
+    //Toggle submit button disable class based on captcha input field.
+    submitBtn.classList.toggle("disabled", !captchaInputBox.value)
+    if (!captchaInputBox.value) message.classList.remove("active");
+
+}
+
+const submitBtnClick = () => {
+    captchaText = captchaText.split("").filter((char => char !== " ")).join(""); 
+    message.classList.add("active");
+    if (captchaInputBox.value === captchaText) {
+        message.innerText = "Entered captcha is correct"
+        message.style.color = "#826afb"
+
+    } else {
+        message.innerText = "Entered captcha is not correct"
+        message.style.color = "#ff2525"
+    }
+};
+
+refreshButton.addEventListener("click", refreshBtnClick);
+captchaInputBox.addEventListener("keyup", captchaKeyUpValidate)
+submitBtn.addEventListener("click",  submitBtnClick   )
+
+//generate a captcha when the page loads
 generateCaptcha();
